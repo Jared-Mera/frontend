6ddca9016fad0ce403e452f3dc5a1d0909c362d4
ElReyDeTepito <126588672+ElReyDeTepito@users.noreply.github.com>
2025-08-20 09:07:01 -0600

    Update ReportFilters.jsx

--- src/components/sales/ReportFilters.jsx
+++ src/components/sales/ReportFilters.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Input from '../ui/Input';
 import Button from '../ui/Button';
 
+#pedro
 const ReportFilters = ({ filters, onFilterChange, onGenerate }) => {
   return (
     <div className="bg-white p-4 rounded-md shadow mb-6">
