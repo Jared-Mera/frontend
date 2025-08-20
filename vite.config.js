1c8c12d809481a63f93c2a0f5cd91bc86f20f0be
Jared Alfonso Mera Hernández <jaredalfonsomerahernandez@gmail.com>
2025-08-20 17:04:02 -0600

    aplication

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // actualiza el service worker automáticamente
+      devOptions: {
+        enabled: true // habilita PWA en modo dev (opcional)
+      },
+      manifest: {
+        name: 'Mi App PWA',
+        short_name: 'MiApp',
+        description: 'Aplicación PWA con Vite + React',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
